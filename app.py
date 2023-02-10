452487924ff6032ed3a65cfccd63cd0d5b94b25f
yannguerin <44235209+yannguerin@users.noreply.github.com>
2023-02-10 09:20:17 -0400

    Added node, edge highlighting on node click

--- app.py
+++ app.py
@@ -9,139 +9,17 @@ from matplotlib import cm
 import networkx as nx
 
 import dash
-import dash_core_components as dcc
 import dash_cytoscape as cyto
 import dash_bootstrap_components as dbc
-from dash import html, Input, Output
+from dash import html, Input, Output, dcc
 
 from neo4j_manager import Neo4jHTTPManager
+from components import *
+
+from nltk.corpus import stopwords
+eng_stopwords = set(stopwords.words('english'))
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
-
-# Database Loading Callback
-# Using the Initial Call
-
-
-# @app.callback(Output("database-loading", 'children'),
-#               Input("depth-slider", "value"))
-# def database_init(value):
-#     if value:
-#         global database
-#         database = Neo4jHTTPManager()
-#         return ""
-
-
-# Layout Components
-
-# General Scaffolding
-
-
-navbar = dbc.NavbarSimple(
-    children=[
-        dbc.NavItem(dbc.NavLink("Page 1", href="/"))
-    ],
-    brand="Concept",
-    brand_href="/",
-    color="dark",
-    links_left=True,
-    dark=True,
-)
-
-# Inputs
-# Word Search
-word_input = html.Div(
-    [
-        dbc.Input(id="word-input", placeholder="Search for Word", type="text"),
-        html.Br(),
-        html.P(id="output"),
-    ],
-    style={"margin": "2% 10% 2% 10%"}
-)
-# Depth Option
-depth_slider = html.Div([
-    html.H6("Pick the Recursive Depth of the Concept Graph",
-            style={"margin-left": "2%"}),
-    dcc.Slider(1, 3, 1,
-               value=2,
-               id='depth-slider'
-               ),
-    dbc.Badge("Chosen Depth of 2", color="info", id='chosen-depth-display')
-],
-    style={"margin": "2% 10% 2% 10%"}
-)
-
-# Generate Button
-
-button_generate = html.Button('Generate Graph', id='generate', n_clicks=0)
-
-# Graph Layout Options
-layout_options = ['concentric', 'breadthfirst', 'circle', 'random']
-graph_layout_options = dcc.Dropdown(
-    layout_options, 'breadthfirst', id='graph-layout-options')
-
-# Cytoscape Graph
-cyto_graph = cyto.Cytoscape(
-    id='cytoscape-graph',
-    style={'width': '80%', 'height': '600px'},
-    minZoom=0.05,
-    maxZoom=10,
-    zoom=1,
-    zoomingEnabled=True,
-    pan={"x": 100, "y": 0},
-    elements=[],
-    layout={
-        "name": "breadthfirst",
-        "roots": "[id = 'defenestration']",
-        # "circle": "false"
-    },
-    stylesheet=[
-        {
-            'selector': 'edge',
-            'style': {
-                'width': 0.1,
-                'mid-target-arrow-shape': 'triangle',
-                'arrow-scale': 1
-            }
-        },
-        {
-            'selector': 'node',
-            'style': {
-                'label': 'data(label)'
-            }
-        }
-    ]
-)
-
-cyto_component = html.Div(cyto_graph,
-                          className="bg-dark rounded-3",
-                          style={"margin": "5% 5% 5% 5%"}
-                          )
-
-
-# Graph Info Components
-clicked_num_connected = html.Pre(
-    id='clicked-num-connected',
-    style={
-        'border': 'thin lightgrey solid',
-        'overflowX': 'scroll'
-    })
-
-
-# default_stylesheet = [
-#     {
-#         "selector": "node",
-#         "style": {
-#             "width": "data(size)",
-#             "height": "data(size)",
-#             "background-color": "mapData(layer, 0, 3, white, blue)",
-#             "content": "data(label)",
-#             "font-size": "12px",
-#             "text-valign": "center",
-#             "text-halign": "center",
-#         },
-#     }
-# ]
-
 
 # Callbacks
 
@@ -158,11 +36,33 @@ def displayDepthOption(depth_value: int):
 # On clicking of node
 
 
-# @app.callback(Output('clicked-num-connected', 'children'),
-#               Input('cytoscape-graph', 'tapNode'))
-# def displayTapNodeData(data: dict):
-#     if data:
-#         return json.dumps(data, indent=2)
+@app.callback(Output('clicked-num-connected', 'children'),
+              Output('cytoscape-graph', 'stylesheet'),
+              Input('cytoscape-graph', 'tapNode'))
+def displayTapNodeData(data: dict):
+    current_style = default_style
+    if data:
+        new_edge_style = [{
+            'selector': f'#{edge["id"]}',
+            'style': {
+                'line-color': ' red',
+                'width': 3
+            }
+        } for edge in data["edgesData"]]
+        new_node_style = [{
+            'selector': f'#{edge["target"]}',
+            'style': {
+                'color': ' red',
+                'background-color': 'red',
+                'width': 100,
+                'height': 100
+            }
+        } for edge in data["edgesData"]]
+        current_style.extend(new_edge_style)
+        current_style.extend(new_node_style)
+        return json.dumps(data, indent=2), current_style
+    else:
+        return "", default_style
 
 # On Panning
 
@@ -178,14 +78,21 @@ def displayPanPosition(value):
 @app.callback(Output("cytoscape-graph", 'elements'),
               Input("generate", 'n_clicks'),
               Input("word-input", "value"),
-              Input("depth-slider", 'value'))
-def generateGraph(n_clicks, word_value, depth_value):
+              Input("depth-slider", 'value'),
+              Input('include-stopwords', 'value'))
+def generateGraph(n_clicks, word_value, depth_value, include_stopwords):
     # Cytoscape Element Generation
     if n_clicks > 0 and word_value and depth_value:
         database = Neo4jHTTPManager()
         initial_value = word_value.strip()
-        word_data = set(database.get_word_data(
-            initial_value, depth_value)[::-1])
+        if not include_stopwords:
+            word_data = database.get_word_data(
+                initial_value, depth_value)[::-1]
+            word_data = set([(source, target) for source,
+                            target in word_data if source not in eng_stopwords and target not in eng_stopwords])
+        else:
+            word_data = set(database.get_word_data(
+                initial_value, depth_value)[::-1])
 
         # Nodes
         nodes = [initial_value]
@@ -217,11 +124,6 @@ def generateGraph(n_clicks, word_value, depth_value):
 #     if layout_option == "breadthfirst" and elements != []:
 #         pass
 
-# BreadthFirst Layout Shifter
-
-def shiftPositions(elem, i):
-    elem.position = {'x': (
-        elem.position['x'] - elem.position['x'] // 4), 'y': (elem.position['y'] - 20)}
 
 # Graph Layout Picker
 
@@ -229,23 +131,40 @@ def shiftPositions(elem, i):
 @app.callback(Output("cytoscape-graph", 'layout'),
               Input("graph-layout-options", 'value'),
               Input('word-input', 'value'),
-              Input('cytoscape-graph', 'layout'))
-def graph_layout_pick(layout_option, word_value, layout):
+              Input('cytoscape-graph', 'layout'),
+              Input('depth-slider', 'value'))
+def graph_layout_pick(layout_option, word_value, layout, depth):
     if layout_option and word_value:
         new_layout = layout
         new_layout['name'] = layout_option
+        new_layout['spacingFactor'] = depth * 5
+        new_layout['avoidOverlap'] = True
+        new_layout['nodeDimensionsIncludeLabels'] = 'true'
         if layout_option == "breadthfirst":
             new_layout['roots'] = f"[id = '{word_value}']"
-            new_layout['avoidOverlap'] = 'true'
+            new_layout['circle'] = True
+            new_layout['padding'] = 50
             # Add position shifting
             # nodes.positions(shiftPositions)
-        elif layout_option == "concentric":
-            new_layout['nodeDimensionsIncludeLabels'] = 'true'
         return new_layout
     else:
         return layout
 
 
+# default_stylesheet = [
+#     {
+#         "selector": "node",
+#         "style": {
+#             "width": "data(size)",
+#             "height": "data(size)",
+#             "background-color": "mapData(layer, 0, 3, white, blue)",
+#             "content": "data(label)",
+#             "font-size": "12px",
+#             "text-valign": "center",
+#             "text-halign": "center",
+#         },
+#     }
+# ]
 # Layout
 app.layout = html.Div(
     style={"backgroundColor": "#FFFFFF"},
@@ -253,6 +172,7 @@ app.layout = html.Div(
         navbar,
         word_input,
         depth_slider,
+        include_stopwords,
         graph_layout_options,
         button_generate,
         cyto_component,
