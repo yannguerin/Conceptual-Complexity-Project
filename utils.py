bcd03c028ecafe20ec8f7b019d9d4ba420ba7dd1
yannguerin <44235209+yannguerin@users.noreply.github.com>
2023-02-13 16:13:31 -0400

    added text cleaning for complexity index

--- utils.py
+++ utils.py
@@ -60,7 +60,8 @@ def definition_word_counter(cleaned_definition: str, remove_stopwords: bool = Tr
 def complexity_index(df, text: str):
     index = 0
     count = 0
-    for word in text.split():
+    cleaned_text = prep_complexity_index_text(text)
+    for word in cleaned_text:
         try:
             index += float(df.frequency[df.word == word])
             count += 1
@@ -69,6 +70,12 @@ def complexity_index(df, text: str):
 
     return index / count if count != 0 else "Error, Attempted Division by Zero"
 
+
+def prep_complexity_index_text(text: str) -> list[str]:
+    no_special_characters = re.sub(":|,|\.|\(|\)", "", text).strip()
+    no_stopwords = [word.lower() for word in no_special_characters.split()
+                    if word not in eng_stopwords]
+    return no_stopwords
 
 # if __name__ == "__main__":
 #     run = Word('run')
