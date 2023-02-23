f4ccd7fc95a8f76e1db9a6d71065150a27adc34c
yannguerin <44235209+yannguerin@users.noreply.github.com>
2023-02-23 09:25:55 -0400

    removed unnecessary code

--- app.py
+++ app.py
@@ -1,17 +1,6 @@
-from collections import Counter
-import json
-import time
-
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-from matplotlib import cm
-import networkx as nx
-
 import dash
-import dash_cytoscape as cyto
 import dash_bootstrap_components as dbc
-from dash import html, Input, Output, dcc
+from dash import html
 
 
 from components import navbar
